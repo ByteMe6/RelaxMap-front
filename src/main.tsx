e91b10757fb91a856812cb13adbb70d3ef4f5e60
ByteMe6 <tsapiy.yurka@gmail.com>
2026-01-20 21:31:49 +0200

    deploy

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,14 @@
-// import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.tsx'
-import { BrowserRouter } from 'react-router-dom'
+// main.tsx або index.tsx
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import { HashRouter } from 'react-router-dom'
+import App from './App'
+import './index.css'
 
-createRoot(document.getElementById('root')!).render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>,
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+    <HashRouter>
+      <App />
+    </HashRouter>
+  </React.StrictMode>,
 )
