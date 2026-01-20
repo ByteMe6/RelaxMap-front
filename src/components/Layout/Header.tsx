66318ecf150044f7f67e245ba356dfff0c6311cf
ByteMe6 <tsapiy.yurka@gmail.com>
2026-01-20 22:11:04 +0200

    header aos

--- src/components/Layout/Header.tsx
+++ src/components/Layout/Header.tsx
@@ -1,19 +1,29 @@
+// Header.tsx
+import { useEffect } from "react";
 import { NavLink } from "react-router-dom";
 import Container from "../Container/Container";
 import styles from "./Header.module.scss";
-// import "../../../node_modules/normalize.css/normalize.css"
 import "normalize.css";
+import AOS from "aos";
+import "aos/dist/aos.css";
 
 function Header() {
+  useEffect(() => {
+    AOS.init({
+      duration: 800,
+      once: true,
+    });
+  }, []);
+
   return (
     <header className={styles.header}>
       <Container isHeader>
-        <div className={styles.logoDiv}>
-          <img src="/assets/logo.png" alt="logo" />
+        <div className={styles.logoDiv} data-aos="fade-down">
+          <img src="assets/logo.png" alt="logo" />
           <span className={styles.logoText}>Relax Map</span>
         </div>
 
-        <div className={styles.half}>
+        <div className={styles.half} data-aos="fade-left">
           <nav className={styles.nav}>
             <ul className={styles.navList}>
               <li className={styles.navListElement}>
@@ -22,7 +32,7 @@ function Header() {
                   className={({ isActive }) => (isActive ? styles.active : "")}
                 >
                   Головна
-                </NavLink>{" "}
+                </NavLink>
               </li>
               <li className={styles.navListElement}>
                 <NavLink
@@ -35,7 +45,7 @@ function Header() {
             </ul>
           </nav>
 
-          <div className={styles.authBox}>
+          <div className={styles.authBox} data-aos="fade-up">
             <button className={styles.loginBtn}>Вхід</button>
             <button className={styles.regBtn}>Регістрація</button>
           </div>
