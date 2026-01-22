5f8ce47e17ea479371c7ba843b6bea5a0d40fc07
SofiaPutlyuk <sofiaputlyuk@gmail.com>
2026-01-22 19:25:00 +0200

    part of work (mobile version)

--- src/pages/LocationFormPage/LocationFormPage.tsx
+++ src/pages/LocationFormPage/LocationFormPage.tsx
@@ -1,4 +1,14 @@
-function LocationFormPage(){
-    return <></>
+import Container from "../../components/Container/Container";
+import styles from "./LocationForm.module.scss"
+import LocationForm from "./LocationForm";
+function LocationFormPage() {
+    return (
+        <Container>
+            <div className={styles.containerNewLocation}>
+                <h1 className={styles.titleNewLocation}>Додавання нового місця</h1>
+                <LocationForm />
+            </div>
+        </Container>
+    )
 }
 export default LocationFormPage;
