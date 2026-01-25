4d910b3fa104736ffd5b35fd8dfdc661b5c318c5
ByteMe6 <tsapiy.yurka@gmail.com>
2026-01-25 12:02:16 +0200

    registration

---
+++ src/pages/Auth/AuthNav.tsx
@@ -0,0 +1,18 @@
+import { Link } from "react-router-dom";
+import styles from "./Auth.module.scss";
+import Container from "../../components/Container/Container";
+
+export default function AuthNav() {
+  return (
+    // <Container>
+      <ul className={styles.linksBox}>
+        <li className={styles.linksBoxItem}>
+          <Link to="/auth/register">Реєстрація</Link>
+        </li>
+        <li className={`${styles.linksBoxItem} ${styles.activeLinkBoxItem}`}>
+          <Link to="/auth/login">Вхід</Link>
+        </li>
+      </ul>
+    // </Container>
+  );
+}
