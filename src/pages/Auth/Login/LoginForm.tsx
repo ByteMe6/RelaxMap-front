68efb098a3bacb17651bab2c4e06ec649b8f3058
ByteMe6 <tsapiy.yurka@gmail.com>
2026-01-30 21:31:36 +0200

    on auth navigate to main page

--- src/pages/Auth/Login/LoginForm.tsx
+++ src/pages/Auth/Login/LoginForm.tsx
@@ -1,46 +1,56 @@
-import { Formik, Form, Field } from "formik";
+import {Formik, Form, Field} from "formik";
 import Container from "../../../components/Container/Container";
 import styles from "../Auth.module.scss";
-import { useAppDispatch, useAppSelector } from "../../../redux/hooks/hook";
-import { loginUser } from "../../../redux/thunk/authThunk";
+import {useAppDispatch, useAppSelector} from "../../../redux/hooks/hook";
+import {loginUser, registerUser} from "../../../redux/thunk/authThunk";
+import {useNavigate} from "react-router-dom";
+
 
 export default function LoginForm() {
-  const dispatch = useAppDispatch();
-  const { loading, error } = useAppSelector((s) => s.auth);
+    const dispatch = useAppDispatch();
+    const {loading, error} = useAppSelector((s) => s.auth);
+    const navigate = useNavigate();
 
-  return (
-    <Container>
-      <Formik
-        initialValues={{ email: "", password: "" }}
-        onSubmit={(values) => dispatch(loginUser(values))}
-      >
-        {() => (
-          <Form className={styles.loginFormContent}>
-            <label htmlFor="email">Пошта*</label>
-            <Field
-              id="email"
-              name="email"
-              placeholder="example@example.com"
-              className={styles.inputField}
-            />
+    return (
+        <Container>
+            <Formik
+                initialValues={{email: "", password: ""}}
+                // onSubmit={(values) => dispatch(loginUser(values))}
+                onSubmit={async (values) => {
+                    const resultAction = await dispatch(loginUser(values));
 
-            <label htmlFor="password">Пароль*</label>
-            <Field
-              id="password"
-              type="password"
-              name="password"
-              placeholder="Password123"
-              className={styles.inputField}
-            />
+                    if (registerUser.fulfilled.match(resultAction)) {
+                        navigate("/");
+                    }
+                }}
+            >
+                {() => (
+                    <Form className={styles.loginFormContent}>
+                        <label htmlFor="email">Пошта*</label>
+                        <Field
+                            id="email"
+                            name="email"
+                            placeholder="example@example.com"
+                            className={styles.inputField}
+                        />
 
-            {error && <p className={styles.error}>{error}</p>}
+                        <label htmlFor="password">Пароль*</label>
+                        <Field
+                            id="password"
+                            type="password"
+                            name="password"
+                            placeholder="Password123"
+                            className={styles.inputField}
+                        />
 
-            <button type="submit" className={styles.btn} disabled={loading}>
-              {loading ? "Завантаження..." : "Увійти"}
-            </button>
-          </Form>
-        )}
-      </Formik>
-    </Container>
-  );
+                        {error && <p className={styles.error}>{error}</p>}
+
+                        <button type="submit" className={styles.btn} disabled={loading}>
+                            {loading ? "Завантаження..." : "Увійти"}
+                        </button>
+                    </Form>
+                )}
+            </Formik>
+        </Container>
+    );
 }
